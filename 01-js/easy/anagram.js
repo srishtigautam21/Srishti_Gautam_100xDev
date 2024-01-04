3d5621e08b0b6851cc86ff13c6933d2f7d6f823d
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2024-01-04 13:11:17 +0530

    01 js anagram

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,21 @@
 */
 
 function isAnagram(str1, str2) {
-
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  let obj1 = {};
+  let obj2 = {};
+  for (let i = 0; i < str1.length; i++) {
+    obj1[str1[i].toLowerCase()] = (obj1[str1[i].toLowerCase()] || 0) + 1;
+    obj2[str2[i].toLowerCase()] = (obj2[str2[i].toLowerCase()] || 0) + 1;
+  }
+  for (let key in obj1) {
+    if (obj1[key] !== obj2[key]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = isAnagram;
