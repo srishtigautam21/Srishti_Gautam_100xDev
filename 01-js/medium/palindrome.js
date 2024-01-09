763414579fc5729186a1f539a04d215332742ce0
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2024-01-09 20:17:51 +0530

    Palindrome code

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -4,9 +4,25 @@
 */
 
 function isPalindrome(str) {
-  return str.toLowerCase() === str.toLowerCase().split("").reverse().join("")
-    ? true
-    : false;
+  let newStr = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== "!" && str[i] !== "," && str[i] !== "?" && str[i] !== ".") {
+      newStr += str[i];
+    }
+  }
+  if (
+    newStr.toLowerCase().split(" ").join("") ===
+    newStr.toLowerCase().split(" ").join("").split("").reverse().join("")
+  )
+    return true;
+  else if (str.toLowerCase() === str.toLowerCase().split("").reverse().join(""))
+    return true;
+  else if (
+    str.toLowerCase().split(" ").join("") ===
+    str.toLowerCase().split(" ").join("").split("").reverse().join("")
+  )
+    return true;
+  else return false;
 }
 
 module.exports = isPalindrome;
